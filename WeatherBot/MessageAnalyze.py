b7e4296f3d31792822056292bffe3b806d1883d9
Zhengming Wang <zmwang622@gmail.com>
2019-01-16 15:08:50 -0500

    Connected NWS and GroupME API

--- WeatherBot/MessageAnalyze.py
+++ WeatherBot/MessageAnalyze.py
@@ -4,9 +4,19 @@ Created on Jan 16, 2019
 @author: mingw
 '''
 import requests
+import time
 
+location_name = 'Pittsburgh, PA'
+location_coords = {'x': '40.45', 'y': '-79.96'}
 request_params = {'token': 'e61b4450fbcc0136c9d45e3bdb947d66'}
-response_messages = requests.get('https://api.groupme.com/v3/groups/41952934/messages', params = request_params).json()['response']['messages']
 
-for message in response_messages:
-    print(message['text'])
+while True:
+    response_messages = requests.get('https://api.groupme.com/v3/groups/41952934/messages', params = request_params).json()['response']['messages']
+    
+    for message in response_messages:
+        if(message['text'] == 'WeatherBot' or message['text']):
+            weather_response = requests.get('https://api.weather.gov/points/' + location_coords['x'] + ',' + location_coords['y'] + '/forecast').json()
+            current_weather = weather_response['properties']['periods'][0]['detailedForecast']
+            print('Weather for ' + location_name + ': \n' + current_weather)
+            break
+    time.sleep(5)
